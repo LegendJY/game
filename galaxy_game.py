e8185a8256a69e9e13b7dba335e997923db6950a
LegendJY <LegendJY731429@gmail.com>
2021-09-30 18:16:43 +0900

    쉴드 추가

--- galaxy_game.py
+++ galaxy_game.py
@@ -10,10 +10,19 @@ img_ship = [ pygame.image.load("img/starship.png"),
              pygame.image.load("img/starship_l.png"),
              pygame.image.load("img/starship_r.png"),
              pygame.image.load("img/starship_burner.png") ]
+
 img_weapon = pygame.image.load("img/bullet.png")
+img_shield = pygame.image.load("img/shield.png")
 
 img_enemy = [ pygame.image.load("img/enemy0.png"),
               pygame.image.load("img/enemy1.png") ]
+
+img_explode = [None,
+               pygame.image.load("img/explosion1.png"),
+               pygame.image.load("img/explosion2.png"),
+               pygame.image.load("img/explosion3.png"),
+               pygame.image.load("img/explosion4.png"),
+               pygame.image.load("img/explosion5.png") ]
 
 ENEMY_MAX = 100
 emy_no = 0
@@ -23,6 +32,8 @@ emy_y = [0] * ENEMY_MAX
 emy_a = [0] * ENEMY_MAX
 emy_type = [0] * ENEMY_MAX
 emy_speed = [0] * ENEMY_MAX
+
+ENEMY_BULLET = 0
 
 LINE_T = -80
 LINE_B = 800
@@ -36,6 +47,8 @@ bg_y = 0
 ship_x = 480
 ship_y = 360
 ship_d = 0
+ship_shield = 100
+ship_muteki = 0
 
 key_spc = 0
 key_spc2 = 0
@@ -43,10 +56,7 @@ key_spc3 = 0
 key_spc4 = 0
 key_spc5 = 0
 
-key_i = 0
-key_j = 0
-key_k = 0
-key_l = 0
+key_z = 0
 MISSILE_MAX = 200
 msl_no = 0
 msl_no2 = 0
@@ -75,9 +85,18 @@ msl_f4 = [False] * MISSILE_MAX
 msl_x4 = [0] * MISSILE_MAX
 msl_y4 = [0] * MISSILE_MAX
 
+EFFECT_MAX = 100
+eff_no = 0
+eff_p = [0] * EFFECT_MAX
+eff_x = [0] * EFFECT_MAX
+eff_y = [0] * EFFECT_MAX
+
+def get_dis(x1,y1,x2,y2):
+    return (x1-x2)**2 + (y1-y2)**2
+
 def move_starship(scrn,key):
-    global ship_x,ship_y,ship_d,missile_move,key_spc,key_spc2,key_spc3,key_spc4,key_spc5,key_i,key_j,key_k,key_l
-    ship_d= 0
+    global ship_x,ship_y,ship_d,missile_move,key_spc,key_spc2,key_spc3,key_spc4,key_spc5,key_z,ship_shield,ship_muteki
+    ship_d = 0
     if key[K_UP] == 1:
         ship_y = ship_y - 20
     if key[K_DOWN] == 1:
@@ -108,18 +127,29 @@ def move_starship(scrn,key):
         set_missile3(0)
         set_missile4(0)
 
-    key_i = (key_i+1) * key[K_i]
-    key_j = (key_j + 1) * key[K_j]
-    key_k = (key_k + 1) * key[K_k]
-    key_l = (key_l + 1) * key[K_l]
-    if key_i == 1:
+    key_z = (key_z+1) * key[K_z]
+    if key_z == 1 and ship_shield > 10:
         set_missile(10)
-    if key_j == 1:
-        set_missile2(10)
-    if key_k == 1:
-        set_missile3(10)
-    if key_l == 1:
-        set_missile4(10)
+        ship_shield -= 10
+    if ship_muteki % 2 == 0:
+        scrn.blit(img_ship[3], [ship_x - 8, ship_y + 40 + (tmr % 3) * 2])
+        scrn.blit(img_ship[ship_d], [ship_x - 37, ship_y - 48])
+    if ship_muteki > 0:
+        ship_muteki -= 1
+        return
+    for i in range(ENEMY_MAX):
+        if emy_f[i] == True:
+            w = img_enemy[emy_type[i]].get_width()
+            h = img_enemy[emy_type[i]].get_height()
+            r = ((w+h)/4 + (74 + 96)/4)
+            if get_dis(emy_x[i],emy_y[i],ship_x,ship_y) < r * r:
+                set_effect(ship_x,ship_y)
+                ship_shield = ship_shield - 10
+                if ship_shield <= 0:
+                    ship_shield = 0
+                if ship_muteki == 0:
+                    ship_muteki = 60
+                emy_f[i] = False
 
     if ship_y < 80:
         ship_y = 80
@@ -129,9 +159,8 @@ def move_starship(scrn,key):
         ship_x = 40
     if ship_x > 920:
         ship_x = 920
-    img_rz = pygame.transform.rotozoom(img_ship[ship_d],0,1)
-    scrn.blit(img_ship[3],[ship_x-9,ship_y + 40+(tmr % 3)*2])
-    scrn.blit(img_rz, [ship_x-37, ship_y-52])
+    scrn.blit(img_ship[3], [ship_x - 8, ship_y + 40 + (tmr % 3) * 2])
+    scrn.blit(img_ship[ship_d], [ship_x - 37, ship_y - 48])
 
 def set_missile(typ):
     global msl_no
@@ -142,7 +171,7 @@ def set_missile(typ):
         msl_a[msl_no] = 270
         msl_no = (msl_no + 1) % MISSILE_MAX
     if typ == 10:
-        for a in range(160,390,10):
+        for a in range(160,550,10):
             msl_f[msl_no] = True
             msl_x[msl_no] = ship_x
             msl_y[msl_no] = ship_y
@@ -150,20 +179,11 @@ def set_missile(typ):
             msl_no = (msl_no + 1) % MISSILE_MAX
 
 def set_missile2(typ):
-    '''global msl_no2
-    if typ == 0:
-        msl_f2[msl_no2] = True
-        msl_x2[msl_no2] = ship_x
-        msl_y2[msl_no2] = ship_y
-        msl_a2[msl_no2] = 270
-        msl_no2 = (msl_no2 + 1) % MISSILE_MAX
-    if typ == 10:
-        for a in range(160, 390, 10):
-            msl_f2[msl_no2] = True
-            msl_x2[msl_no2] = ship_x
-            msl_y2[msl_no2] = ship_y
-            msl_a2[msl_no2] = a
-            msl_no2 = (msl_no2 + 1) % MISSILE_MAX'''
+    global msl_no2
+    msl_f2[msl_no2] = True
+    msl_x2[msl_no2] = ship_x
+    msl_y2[msl_no2] = ship_y
+    msl_no2 = (msl_no2 + 1) % MISSILE_MAX
 
 def set_missile3(typ):
     global msl_no3
@@ -190,17 +210,17 @@ def move_missile(scrn):
             if msl_y[i] < 0 or msl_x[i] < 0 or msl_x[i] > 960:
                 msl_f[i] = False
 
+
 def move_missile2(scrn):
-    '''global msl_f2, msl_y2
+    global msl_f2, msl_y2
     for i in range(MISSILE_MAX):
         if msl_f2[i] == True:
-            msl_x2[i] += 50 * math.cos(math.radians(msl_a2[i]))
-            msl_y2[i] += 50 * math.sin(math.radians(msl_a2[i]))
-            img_rz6 = pygame.transform.rotozoom(img_weapon, -90 - msl_a2[i], 1.0)
-            scrn.blit(img_rz6, [msl_x2[i] - img_rz6.get_width() / 2, msl_y2[i] - img_rz6.get_height() / 2])
-            if msl_y2[i] < 0 or msl_x2[i] < 0 or msl_x2[i] > 960:
+            msl_y2[i] += 50
+            img_rz2 = pygame.transform.rotozoom(img_weapon, 180, 1.0)
+            scrn.blit(img_rz2, [msl_x2[i] - 10, msl_y2[i] - 60])
+            if msl_x2[i] < 0:
                 msl_f2[i] = False
-'''
+
 def move_missile3(scrn):
     global msl_f3, msl_y3
     for i in range(MISSILE_MAX):
@@ -226,33 +246,82 @@ def bring_enemy():
     if tmr % 30 == 0:
         set_enemy(random.randint(20,940),LINE_T,90,1,6)
 
-def set_enemy(x,y,a,ty,sp):
+def set_enemy(x, y, a, ty, sp): # 적 기체 설정
     global emy_no
     while True:
-        if emy_f[emy_no] == False:
+        if emy_f[emy_no] == False :
             emy_f[emy_no] = True
             emy_x[emy_no] = x
             emy_y[emy_no] = y
             emy_a[emy_no] = a
             emy_type[emy_no] = ty
             emy_speed[emy_no] = sp
+            break
+        emy_no = (emy_no+1) % ENEMY_MAX
 
 def move_enemy(scrn):
+    global ship_shield
     for i in range(ENEMY_MAX):
-        if emy_f[i] == True:
-            ang = -90 - emy_a
-            png = emy_type[i]
-            emy_x = emy_x[i] + emy_speed[i] * math.cos(math.radians(emy_a[i]))
-            emy_y = emy_y[i] + emy_speed[i] * math.sin(math.radians(emy_a[i]))
-            if emy_type == 1 and emy_y[i] > 360:
-                set_enemy(emy_x[i],emy_y[i],90,0,8)
-                emy_a[i] -45
-                emy_speed[i] = 16
+        if emy_f[i] == True :
+            ang = -90 - emy_a[i]
+            png =  emy_type[i]
+            emy_x[i] = emy_x[i] + emy_speed[i] * math.cos(math.radians(emy_a[i]))
+            emy_y[i] = emy_y[i] + emy_speed[i] * math.sin(math.radians(emy_a[i]))
+            if emy_type[i] == 1 and emy_y[i] > 360:
+                set_enemy(emy_x[i], emy_y[i], 90, 0, 8)
+                emy_a[i] = -10
+                emy_speed[i] = 100
 
-            if emy_x[i] < LINE_L or LINE_R < emy_x[i] or emy_y[i] < LINE_T or LINE_B < emy_y[i]:
+            if emy_x[i] < LINE_L or LINE_R < emy_x[i] or emy_y[i] < LINE_T or LINE_B < emy_y[i] :
                 emy_f[i] = False
-            img_rz = pygame.transform.rotozoom(img_enemy[png],ang,1.0)
-            scrn.blit(img_rz,[emy_x[i]-img_rz.get_width()/2,emy_y[i]-img_rz.get_height()/2])
+
+            if emy_type[i] != ENEMY_BULLET:
+                w = img_enemy[emy_type[i]].get_width()
+                h = img_enemy[emy_type[i]].get_height()
+                r = int(w+h / 4) + 12
+                for n in range(MISSILE_MAX):
+                    if msl_f[n] == True and get_dis(emy_x[i],emy_y[i],msl_x[n],msl_y[n]) < r*r:
+                        msl_f[n] = False
+                        set_effect(emy_x[i],emy_y[i])
+                        emy_f[i] = False
+                        if ship_shield < 100:
+                            ship_shield += 1
+                    if msl_f2[n] == True and get_dis(emy_x[i],emy_y[i],msl_x2[n],msl_y2[n]) < r*r:
+                        msl_f2[n] = False
+                        set_effect(emy_x[i],emy_y[i])
+                        emy_f[i] = False
+                        if ship_shield < 100:
+                            ship_shield += 1
+                    if msl_f3[n] == True and get_dis(emy_x[i],emy_y[i],msl_x3[n],msl_y3[n]) < r*r:
+                        msl_f3[n] = False
+                        set_effect(emy_x[i],emy_y[i])
+                        emy_f[i] = False
+                        if ship_shield < 100:
+                            ship_shield += 1
+                    if msl_f4[n] == True and get_dis(emy_x[i],emy_y[i],msl_x4[n],msl_y4[n]) < r*r:
+                        msl_f4[n] = False
+                        set_effect(emy_x[i],emy_y[i])
+                        emy_f[i] = False
+                        if ship_shield < 100:
+                            ship_shield += 1
+
+            img_rz = pygame.transform.rotozoom(img_enemy[png], ang, 1.0)
+            scrn.blit(img_rz, [emy_x[i] - img_rz.get_width() / 2, emy_y[i] - img_rz.get_height() /2] )
+
+def set_effect(x,y):
+    global eff_no
+    eff_p[eff_no] = 1
+    eff_x[eff_no] = x
+    eff_y[eff_no] = y
+    eff_no = (eff_no + 1) % EFFECT_MAX
+
+def draw_effect(scrn):
+    for i in range(EFFECT_MAX):
+        if eff_p[i] > 0:
+            scrn.blit(img_explode[eff_p[i]],[eff_x[i]-48,eff_y[i]-48])
+            eff_p[i] += 1
+            if eff_p[i] == 6:
+                eff_p[i] = 0
 
 def main():
     global bg_y,tmr
@@ -286,7 +355,9 @@ def main():
         move_missile4(screen)
         bring_enemy()
         move_enemy(screen)
-
+        draw_effect(screen)
+        screen.blit(img_shield,[40,680])
+        pygame.draw.rect(screen,(64,32,32),[40+ship_shield * 4,680,(100 - ship_shield) * 4 ,12])
         pygame.display.update()
         clock.tick(20)
 
